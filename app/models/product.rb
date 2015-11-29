33dbe8de5c58e65ccd49c23a56c3ecab312add01
Haiyang (harry) Gao <foxgaocn@gmail.com>
2015-11-29 17:43:27 +1100

    remove

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,4 @@
 class Product < ActiveRecord::Base
-  has_many :prices, -> { order "created_at DESC" }
-  belongs_to :category
-  validates :name, :url, presence: true
-
-  def current_price
-    @current_price ||= prices.first.value
-  end
-
+  #belongs_to :category
+  validates :name, :url, :img, :price, presence: true
 end
