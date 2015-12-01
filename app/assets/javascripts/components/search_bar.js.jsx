f8c0cf9f6636002ab679f5af18d8fe067220634d
Haiyang (harry) Gao <foxgaocn@gmail.com>
2015-12-02 09:51:09 +1100

    fix input

--- app/assets/javascripts/components/search_bar.js.jsx
+++ app/assets/javascripts/components/search_bar.js.jsx
@@ -1,17 +1,15 @@
 var SearchBar = React.createClass({
-  propTypes: {
-  },
-
+  
   keyDown: function(event){
-       if(event.keyCode == 13){
-          this.props.history.pushState(null, '/search', {term: this.refs.term.value})
-       }
-   },
+    if(event.keyCode == 13){
+      this.props.history.pushState(null, '/search', {term: this.refs.term.value})
+    }
+  },
 
   render: function() {
     return (
       <div className="row searchBar">
-        <input type="search" ref="term" value={this.props.term} className="form-control" placeholder="search product here" onKeyDown={this.keyDown}/>
+        <input type="search" ref="term" defaultValue={this.props.term} className="form-control" placeholder="search product here" onKeyDown={this.keyDown}/>
       </div>
     );
   }
