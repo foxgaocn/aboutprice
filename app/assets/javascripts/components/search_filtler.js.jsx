7a81ca8def786dfbad498e92730760e2365143c6
Haiyang (harry) Gao <foxgaocn@gmail.com>
2015-12-06 18:45:13 +1100

    filter

--- app/assets/javascripts/components/search_filtler.js.jsx
+++ app/assets/javascripts/components/search_filtler.js.jsx
@@ -24,15 +24,21 @@ var SearchFilter = React.createClass({
   },
 
   smallFilter: function(filter){
+    expand = <div/>
+    hide = <div/>
+    if(this.state.filterContentClass == 'hidden')
+      expand = <div className='text-center'> <a href="#" id="refinementsToggle" onClick={this.toggleRefinements} className={this.state.filterToggleClass}><span>Show filter</span></a></div>
+    else
+      hide = <div className='text-center'> <a href="#" id="refinementsToggle" onClick={this.toggleRefinements} className={this.state.filterToggleClass}><span>Hide filter</span></a></div>
+    
     return(
       <div className="filter-small">
-        <div className='text-center'>
-          <a href="#" id="refinementsToggle" onClick={this.toggleRefinements} className={this.state.filterToggleClass}><span>{this.state.refinementsText}</span></a>
-        </div>
+        {expand}
         <div className={this.state.filterContentClass}>
           <SearchCategories history={this.props.history} categories={this.props.categories} filter={filter.category_id} onFilterChange={this.props.onFilterChange}/>
           <SearchShops history={this.props.history} shops={this.props.shops} filter={filter.shop_ids} onFilterChange={this.props.onFilterChange}/>
         </div>
+        {hide}
       </div>
       )
   },
