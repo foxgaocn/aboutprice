af2a1be63647f24831c62f09a35382a834bf6ade
Haiyang (harry) Gao <foxgaocn@gmail.com>
2015-12-28 21:35:35 +1100

    disable auto complete

--- app/assets/javascripts/components/search_bar.js.jsx
+++ app/assets/javascripts/components/search_bar.js.jsx
@@ -23,7 +23,7 @@ var SearchBar = React.createClass({
   render: function() {
     return (
       <div className="input-group col-sm-12 col-md-8 col-md-offset-2" id="search-bar">
-        <input type="search" ref="term" onChange={this.termChange} defaultValue={this.props.term} className="form-control" placeholder="search product here" onKeyDown={this.keyDown}/>
+        <input type="search" ref="term" onChange={this.termChange} defaultValue={this.props.term} className="form-control" placeholder="search product here" onKeyDown={this.keyDown} autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"/>
         <span className="input-group-btn">
           <button className="btn btn-default" type="button" onClick={this.performSearch}>
             <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
