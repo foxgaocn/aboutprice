d5582ab7117e874b5fc10dab7928e68e4defcb76
Haiyang (harry) Gao <foxgaocn@gmail.com>
2015-12-29 17:08:22 +1100

    task to regenerate  config

--- tasks.sh
+++ tasks.sh
@@ -2,5 +2,6 @@
 cd /home/deploy/pricepal/scrapper
 ./bin/consloe production
 cd /data/aboutprice/current
+RAILS_ENV=production INDEX_ONLY=true bundle exec rake ts:configure
 RAILS_ENV=production INDEX_ONLY=true bundle exec rake ts:index
 RAILS_ENV=production bundle exec rake top
