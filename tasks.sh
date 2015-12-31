b9483d4d663a059d12e114672c515aad8188be03
Haiyang (harry) Gao <foxgaocn@gmail.com>
2015-12-31 19:30:58 +1100

    change task shell

--- tasks.sh
+++ tasks.sh
@@ -2,6 +2,8 @@
 cd /home/deploy/pricepal/scrapper
 ./bin/consloe production
 cd /data/aboutprice/current
-RAILS_ENV=production INDEX_ONLY=true bundle exec rake ts:configure
+RAILS_ENV=production bundle exec rake ts:configure
 RAILS_ENV=production INDEX_ONLY=true bundle exec rake ts:index
+kill -9 $(pidof searchd)
+RAILS_ENV=production bundle exec rake ts:start
 RAILS_ENV=production bundle exec rake top
